938a0a16a72b5f103098da0d72285d3636cc4431
Jahrel Balon <balonj@uspioneer.com>
2024-01-28 20:26:34 -0800

    [main] Added quiz scoring unit test.

---
+++ QuizWebsite/Assemblies/QuizWebsite.Web.Test/QuizScorerUnitTests.cs
@@ -0,0 +1,79 @@
+using System;
+using Bogus;
+using QuizWebsite.Core.Models;
+using QuizWebsite.Pages;
+using QuizWebsite.Web.Pages;
+
+namespace QuizWebsite.Web.Test
+{
+    [TestClass]
+    public partial class QuizScorerUnitTests
+    {
+        public Faker FakerInstance { get; set; }
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            FakerInstance = new Faker();
+        }
+
+        private delegate (int CorrectCount, int TotalCount) SolvingAlgorithm(Quiz quiz, List<QuizAnswersViewModel> answers);
+
+        [TestMethod]
+        public void TestDatAlgo()
+        {
+            int testsToRun = 500000;
+            int questionsInQuiz = 50;
+            int targetQuizCorrectnessInGeneratedAnswers = 50;
+            SolvingAlgorithm solvingAlgo = null; //-- Put your algo here!
+
+            Console.WriteLine($"Test Parameters:");
+            Console.WriteLine($"\tTests to run: {testsToRun}");
+            Console.WriteLine($"\tQuestions in quiz: {questionsInQuiz}");
+            Console.WriteLine($"\tTarget quiz correctness in generated answers: {targetQuizCorrectnessInGeneratedAnswers}%");
+            Console.WriteLine($"\tAlgorithm: {solvingAlgo.Method.DeclaringType.Name}.{solvingAlgo.Method.Name}()");
+            Console.WriteLine(string.Empty);
+
+            var stopwatch = new System.Diagnostics.Stopwatch();
+            stopwatch.Start();
+
+            var quiz = GenerateQuiz(questionsInQuiz);
+            var answerSets = new List<List<QuizAnswersViewModel>>();
+            while (answerSets.Count < testsToRun)
+            {
+                int diceRollForCorrect = FakerInstance.Random.Int(0, 100);
+                bool shouldBeCorrect = targetQuizCorrectnessInGeneratedAnswers >= diceRollForCorrect;
+
+                if (shouldBeCorrect)
+                {
+                    answerSets.Add(GenerateAnswers(quiz, 100));
+                }
+                else
+                {
+                    answerSets.Add(GenerateAnswers(quiz, FakerInstance.Random.Int(0, 50)));
+                }
+            }
+
+            stopwatch.Stop();
+            Console.WriteLine($"Time to generate scenarios: {stopwatch.Elapsed.ToString("mm\\:ss\\.fffff")}");
+            stopwatch.Reset();
+
+            stopwatch.Start();
+
+            int countOfCorrectQuizes = 0;
+            foreach (var answerSet in answerSets)
+            {
+                var score = solvingAlgo(quiz, answerSet);
+                if (score.CorrectCount == score.TotalCount)
+                    countOfCorrectQuizes++;
+            }
+
+            stopwatch.Stop();
+
+            Console.WriteLine($"Correct quizzes: {countOfCorrectQuizes.ToString()} / {testsToRun} ({((double)countOfCorrectQuizes / testsToRun * 100).ToString("0.00")}%)");
+            Console.WriteLine($"Total solve time: {stopwatch.Elapsed.ToString("mm\\:ss\\.fffff")}");
+            var averageTime = new TimeSpan(stopwatch.ElapsedTicks / testsToRun);
+            Console.WriteLine($"Average time per quiz solve: {averageTime.ToString("mm\\:ss\\.fffff")}");
+        }
+    }
+}
---
+++ QuizWebsite/Assemblies/QuizWebsite.Web.Test/QuizScorerUnitTests.utilities.cs
@@ -0,0 +1,306 @@
+using Bogus;
+using QuizWebsite.Core.Models;
+using QuizWebsite.Core.Models.Base;
+using QuizWebsite.Pages;
+
+namespace QuizWebsite.Web.Test
+{
+    public partial class QuizScorerUnitTests
+    {
+        private const string GeneratedAuthorName = "QuizScorerFidelityUnitTests";
+        private const string SingleSelectQuestionTypeName = "single_select";
+        private const string MultiSelectQuestionTypeName = "multi_select";
+        private const string FreeResponseQuestionTypeName = "free_response";
+        private const string FillInBlankQuestionTypeName = "fill_in_blank";
+
+        private Quiz GenerateQuiz(int quizQuestions = 100)
+        {
+            var quiz = new Quiz()
+            {
+                Title = "GenerateQuiz",
+                Author = GeneratedAuthorName,
+                CreatedTimestamp = DateTime.Now,
+            };
+
+            int currentQuestionId = 1;
+            string[] questionTypes = { SingleSelectQuestionTypeName, MultiSelectQuestionTypeName, FreeResponseQuestionTypeName };
+
+            while (quiz.Questions.Count() < quizQuestions)
+            {
+                string selectedQuestionType = FakerInstance.PickRandom(questionTypes);
+                switch (selectedQuestionType)
+                {
+                    case SingleSelectQuestionTypeName:
+                        quiz.Questions.Add(GenerateRadioButtonQuestion(currentQuestionId));
+                        break;
+                    case MultiSelectQuestionTypeName:
+                        quiz.Questions.Add(GenerateCheckboxQuestion(currentQuestionId));
+                        break;
+                    case FreeResponseQuestionTypeName:
+                        quiz.Questions.Add(GenerateTextQuestion(currentQuestionId));
+                        break;
+                }
+
+                currentQuestionId++;
+            }
+
+            return quiz;
+        }
+
+        private Quiz GenerateRadioButtonOnlyQuiz()
+        {
+            var quiz = new Quiz()
+            {
+                Title = "GenerateRadioButtonOnlyQuiz",
+                Author = GeneratedAuthorName,
+                CreatedTimestamp = DateTime.Now,
+            };
+
+            int currentQuestionId = 1;
+
+            while (quiz.Questions.Count() < 10)
+            {
+                quiz.Questions.Add(GenerateRadioButtonQuestion(currentQuestionId));
+
+                currentQuestionId++;
+            }
+
+            return quiz;
+        }
+
+        private Quiz GenerateCheckboxOnlyQuiz()
+        {
+            var quiz = new Quiz()
+            {
+                Title = "GenerateCheckboxOnlyQuiz",
+                Author = GeneratedAuthorName,
+                CreatedTimestamp = DateTime.Now,
+            };
+
+            int currentQuestionId = 1;
+
+            while (quiz.Questions.Count() < 10)
+            {
+                quiz.Questions.Add(GenerateCheckboxQuestion(currentQuestionId));
+
+                currentQuestionId++;
+            }
+
+            return quiz;
+        }
+
+        private Quiz GenerateTextboxOnlyQuiz()
+        {
+            var quiz = new Quiz()
+            {
+                Title = "GenerateTextboxOnlyQuiz",
+                Author = GeneratedAuthorName,
+                CreatedTimestamp = DateTime.Now,
+            };
+
+            long currentQuestionId = 1;
+
+            while (quiz.Questions.Count() < 10)
+            {
+                quiz.Questions.Add(GenerateTextQuestion(currentQuestionId));
+
+                currentQuestionId++;
+            }
+
+            return quiz;
+        }
+
+        private SelectQuestion GenerateRadioButtonQuestion(long currentQuestionId)
+        {
+            var selectQuestion = new SelectQuestion()
+            {
+                QuestionId = currentQuestionId,
+                QuestionText = $"Question {currentQuestionId}. What is the answer?",
+                QuestionTypeName = SingleSelectQuestionTypeName
+            };
+
+            int currentAnswerOptionId = 1;
+            int maxOptions = FakerInstance.Random.Int(2, 4);
+            int correctOptionId = FakerInstance.Random.Int(currentAnswerOptionId, maxOptions);
+
+            while (selectQuestion.AnswerOptions.Count() < maxOptions)
+            {
+                var answerOption = new AnswerOption()
+                {
+                    Id = currentAnswerOptionId,
+                    IsCorrect = correctOptionId == currentAnswerOptionId,
+                    OptionText = FakerInstance.Random.Word()
+                };
+                selectQuestion.AnswerOptions.Add(answerOption);
+
+                currentAnswerOptionId++;
+            }
+
+            return selectQuestion;
+        }
+
+        private SelectQuestion GenerateCheckboxQuestion(long currentQuestionId)
+        {
+            var selectQuestion = new SelectQuestion()
+            {
+                QuestionId = currentQuestionId,
+                QuestionText = $"Question {currentQuestionId}. What is the answer?",
+                QuestionTypeName = MultiSelectQuestionTypeName
+            };
+
+            int currentAnswerOptionId = 1;
+            int maxOptions = FakerInstance.Random.Int(5, 10);
+
+            while (selectQuestion.AnswerOptions.Count() < maxOptions)
+            {
+                var answerOption = new AnswerOption()
+                {
+                    Id = currentAnswerOptionId,
+                    IsCorrect = FakerInstance.Random.Bool(),
+                    OptionText = FakerInstance.Random.Word()
+                };
+                selectQuestion.AnswerOptions.Add(answerOption);
+
+                currentAnswerOptionId++;
+            }
+
+            return selectQuestion;
+        }
+
+        private TextQuestion GenerateTextQuestion(long currentQuestionId)
+        {
+            return new TextQuestion()
+            {
+                QuestionId = currentQuestionId,
+                QuestionText = $"Question {currentQuestionId}. What is the answer?",
+                QuestionTypeName = FreeResponseQuestionTypeName,
+                AnswerText = FakerInstance.Random.Words(FakerInstance.Random.Int(1, 5))
+            };
+        }
+
+        private List<QuizAnswersViewModel> GenerateAnswers(Quiz quiz, int percentageCorrect)
+        {
+            var answers = new List<QuizAnswersViewModel>();
+
+            foreach (var question in quiz.Questions)
+            {
+                var response = new QuizAnswersViewModel()
+                {
+                    QuestionId = question.QuestionId
+                };
+
+                int diceRollForCorrect = FakerInstance.Random.Int(0, 100);
+                bool shouldBeCorrect = percentageCorrect >= diceRollForCorrect;
+
+                switch (question.QuestionTypeName)
+                {
+                    case SingleSelectQuestionTypeName:
+                        response.Answers = GenerateRadioButtonAnswers((SelectQuestion)question, shouldBeCorrect);
+                        break;
+                    case MultiSelectQuestionTypeName:
+                        response.Answers = GenerateCheckboxAnswers((SelectQuestion)question, shouldBeCorrect);
+                        break;
+                    case FreeResponseQuestionTypeName:
+                        response.Answers = GenerateTextAnswer((TextQuestion)question, shouldBeCorrect);
+                        break;
+                }
+
+                answers.Add(response);
+            }
+
+            return answers;
+        }
+
+        private List<string> GenerateRadioButtonAnswers(SelectQuestion question, bool isCorrect)
+        {
+            var answers = new List<string>();
+
+            if (isCorrect)
+            {
+                var correctAnswerOption = question.AnswerOptions.FirstOrDefault(x => x.IsCorrect);
+                answers.Add(correctAnswerOption.Id.ToString());
+            }
+            else
+            {
+                if (FakerInstance.Random.Bool()) //-- Chance to not have any answer
+                {
+                    var correctAnswerOption = question.AnswerOptions.FirstOrDefault(x => x.IsCorrect);
+
+                    string wrongAnswer;
+                    do
+                    {
+                        wrongAnswer = FakerInstance.Random.Int((int)question.AnswerOptions.Min(x => x.Id), (int)question.AnswerOptions.Max(x => x.Id)).ToString();
+                    }
+                    while (wrongAnswer == correctAnswerOption.Id.ToString());
+                    answers.Add(wrongAnswer);
+                }
+            }
+
+            return answers;
+        }
+
+        private List<string> GenerateCheckboxAnswers(SelectQuestion question, bool isCorrect)
+        {
+            var answers = new List<string>();
+
+            if (isCorrect)
+            {
+                foreach (var answerOption in question.AnswerOptions)
+                {
+                    if (answerOption.IsCorrect)
+                        answers.Add(answerOption.Id.ToString());
+                    else
+                        answers.Add("false");
+                }
+            }
+            else
+            {
+                foreach (var answerOption in question.AnswerOptions)
+                {
+                    if (FakerInstance.Random.Bool()) //-- Chance to not have any answer
+                    {
+                        string wrongAnswer;
+                        do
+                        {
+                            if (FakerInstance.Random.Bool())
+                            {
+                                wrongAnswer = FakerInstance.Random.Int((int)question.AnswerOptions.Min(x => x.Id), (int)question.AnswerOptions.Max(x => x.Id)).ToString();
+                            }
+                            else
+                                wrongAnswer = "false";
+                        }
+                        while (question.AnswerOptions.Where(x => x.IsCorrect).Select(x => x.Id.ToString()).Contains(wrongAnswer));
+                        answers.Add(wrongAnswer);
+                    }
+                }
+            }
+
+            return answers;
+        }
+
+        private List<string> GenerateTextAnswer(TextQuestion question, bool isCorrect)
+        {
+            var answers = new List<string>();
+
+            if (isCorrect)
+            {
+                answers.Add(question.AnswerText);
+            }
+            else
+            {
+                if (FakerInstance.Random.Bool()) //-- Chance to not have any answer
+                {
+                    string wrongAnswer;
+                    do
+                    {
+                        wrongAnswer = FakerInstance.Random.Word();
+                    }
+                    while (wrongAnswer == question.AnswerText);
+                    answers.Add(wrongAnswer);
+                }
+            }
+
+            return answers;
+        }
+    }
+}
