d5cf191d9ecaabdf5ad890004b3f746673e9b852
Jahrel Balon <balonj@uspioneer.com>
2024-01-28 22:23:42 -0800

    [main] Added my algo.

---
+++ QuizWebsite/Assemblies/QuizWebsite.Web/Pages/SolverBoiOne.cs
@@ -0,0 +1,50 @@
+﻿using QuizWebsite.Core.Models;
+using QuizWebsite.Core.Models.Base;
+using QuizWebsite.Pages;
+
+namespace QuizWebsite.Web.Pages
+{
+    public static class SolverBoiOne
+    {
+        public static (int CorrectCount, int TotalCount) Solve(Quiz quiz, List<QuizAnswersViewModel> answers)
+        {
+            var evaluationPairList = new List<(QuizQuestion Question, bool Correct)>();
+
+            foreach (var question in quiz.Questions)
+            {
+                if (question is SelectQuestion)
+                {
+                    var selectQuestion = (SelectQuestion)question;
+                    var matchingResponse = answers.FirstOrDefault(x => x.QuestionId == selectQuestion.QuestionId);
+                    if (matchingResponse != null)
+                    {
+                        matchingResponse.Answers.RemoveAll(x => x == "false");
+                        if (matchingResponse.Answers.SequenceEqual(selectQuestion.AnswerOptions.Where(x => x.IsCorrect).Select(x => x.Id.ToString())))
+                        {
+                            evaluationPairList.Add((selectQuestion, true));
+                            continue;
+                        }
+                    }
+                }
+                else if (question is TextQuestion)
+                {
+                    var textQuestion = (TextQuestion)question;
+                    var matchingResponse = answers.FirstOrDefault(x => x.QuestionId == textQuestion.QuestionId);
+                    if (matchingResponse != null && matchingResponse.Answers.FirstOrDefault() == textQuestion.AnswerText)
+                    {
+                        evaluationPairList.Add((textQuestion, true));
+                        continue;
+                    }
+                }
+                else
+                    throw new NotSupportedException("Question type isn't supported.");
+
+                evaluationPairList.Add((question, false));
+            }
+
+            long questionId = 0;
+
+            return (CorrectCount: evaluationPairList.Count(x => x.Correct), TotalCount: evaluationPairList.Count());
+        }
+    }
+}
---
+++ QuizWebsite/Assemblies/QuizWebsite.Web/Pages/SolverTwoOne.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections.Concurrent;
+using QuizWebsite.Core.Models;
+using QuizWebsite.Core.Models.Base;
+using QuizWebsite.Pages;
+
+namespace QuizWebsite.Web.Pages
+{
+    public static class SolverBoiTwo
+    {
+        public static (int CorrectCount, int TotalCount) Solve(Quiz quiz, List<QuizAnswersViewModel> answers)
+        {
+            var evaluationPairList = new ConcurrentBag<(QuizQuestion Question, bool Correct)>();
+            var tasks = new List<Task>();
+
+            foreach (var question in quiz.Questions)
+            {
+                tasks.Add(Task.Run(() => {
+                    evaluationPairList.Add((question, EvaluateQuestion(question, answers)));
+                }));
+            }
+
+            var task = Task.WhenAll(tasks);
+            task.Wait();
+
+            return (CorrectCount: evaluationPairList.Count(x => x.Correct), TotalCount: evaluationPairList.Count());
+        }
+
+        private static bool EvaluateQuestion(QuizQuestion question, List<QuizAnswersViewModel> answers)
+        {
+            if (question is SelectQuestion)
+            {
+                var selectQuestion = (SelectQuestion)question;
+                var matchingResponse = answers.FirstOrDefault(x => x.QuestionId == selectQuestion.QuestionId);
+                if (matchingResponse != null)
+                {
+                    matchingResponse.Answers.RemoveAll(x => x == "false");
+                    if (matchingResponse.Answers.SequenceEqual(selectQuestion.AnswerOptions.Where(x => x.IsCorrect).Select(x => x.Id.ToString())))
+                    {
+                        return true;
+                    }
+                }
+            }
+            else if (question is TextQuestion)
+            {
+                var textQuestion = (TextQuestion)question;
+                var matchingResponse = answers.FirstOrDefault(x => x.QuestionId == textQuestion.QuestionId);
+                if (matchingResponse != null && matchingResponse.Answers.FirstOrDefault() == textQuestion.AnswerText)
+                {
+                    return true;
+                }
+            }
+            else
+                throw new NotSupportedException("Question type isn't supported.");
+
+            return false;
+        }
+    }
+}
