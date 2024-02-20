e335f1cc414b73b18c2fa05627520eae5564b58d
Jahrel Balon <balonj@uspioneer.com>
2024-02-19 23:40:23 -0800

    [main] Added MockupQuiz.html.

--- QuizWebsite/Assemblies/QuizWebsite.Web/wwwroot/js/site.js
+++ QuizWebsite/Assemblies/QuizWebsite.Web/wwwroot/js/site.js
@@ -1,4 +1,4 @@
-﻿const { Alert } = require("../lib/bootstrap/dist/js/bootstrap.bundle");
+﻿//const { Alert } = require("../lib/bootstrap/dist/js/bootstrap.bundle"); //-- I commented it out because some stuff broke for me - JB
 
 function searchUser() {
     let params = { searchedUser: document.getElementById("txtSearch").value };
@@ -99,4 +99,23 @@ function showUserModal() {
 
         }
     });
+}
+
+var clockInterval;
+var clockStartTime;
+var $clock;
+
+function startClock(elementSelector) {
+    $clock = $("#" + elementSelector);
+
+    if ($clock.length === 0)
+        return;
+        
+    clockStartTime = new Date();
+
+    clockInterval = setInterval(function () {
+        let now = Date.now();
+        let delta = now - clockStartTime;
+        $clock.text(delta / 1000);
+    }, 33.33);
 }
